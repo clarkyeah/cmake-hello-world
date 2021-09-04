cea57df3de54e9b806fa97016507307a3e381df7
clark <190022336@qq.com>
2021-09-04 12:58:36 +0800

    add sonarstage

---
+++ sonar.sh
@@ -0,0 +1,7 @@
+sonar-scanner \
+ -Dsonar.organization=clarkyeah \ 
+ -Dsonar.projectKey=clark13437187167 \
+ -Dsonar.sources=. \
+ -Dsonar.cfamily.build-wrapper-output=bw-output\
+ -Dsonar.host.url=https://sonarcloud.io \
+ -Dsonar.login=27709c1aaae41e3e1858153d3fb8e77f0d38273d
