4aa45a891666afedd2db951483d8848533f41a4e
clarkyeah <clarkyeah@gmail.com>
2021-09-04 13:34:53 +0800

    Delete sonar.sh

--- sonar.sh
+++
@@ -1,7 +0,0 @@
-sonar-scanner \
- -Dsonar.organization=clarkyeah \ 
- -Dsonar.projectKey=hello_jenkins_pipeline \
- -Dsonar.sources=. \
- -Dsonar.cfamily.build-wrapper-output=bw-output\
- -Dsonar.host.url=https://sonarcloud.io \
- -Dsonar.login=27709c1aaae41e3e1858153d3fb8e77f0d38273d
