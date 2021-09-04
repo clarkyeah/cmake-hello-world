f2088b59396d20648c7a45789d1a64450ffac181
clark <190022336@qq.com>
2021-09-04 09:50:16 +0800

    modify sh

--- build_wrapper.sh
+++ build_wrapper.sh
@@ -1,3 +1,9 @@
-export PATH=$PATH:./build-wrapper-linux-x86
-cd ./build-wrapper-linux-x86
-build-wrapper-linux-x86-64 --out-dir ../ make
+ls
+rm -rf build
+mkdir build
+cd build
+cmake ..
+cd ..
+curl -L -O https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+unzip -o build-wrapper-linux-x86.zip
+./build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output cmake --build build --config Release
